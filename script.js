7f9de250241b3fa8f85d392fe29f8467958a5366
Het <het.parekh@somaiya.edu>
2021-10-09 04:32:56 +0530

    Code Merged

--- script.js
+++ script.js
@@ -1,59 +1,72 @@
 // Navbar
 $(document).ready(function () {
 
-  $('.dropdown-item').on("click", function (e) {
+  $('.dropdown-item').on("click", function(e){
     e.stopPropagation();
     e.preventDefault();
   });
 
-  $(".nav-link").click(function () {
-    $(this).toggleClass('addcolor')
-  })
-  $(".nav-link").blur(function () {
-    $(this).removeClass('addcolor')
-  })
+  $("#column_left").hide()
 
-  $('.multilevel').each(function () {
-    let val = $($(this)).attr('id')
-
-    $(`#${val}`).mouseenter(function () {
-      $(this).attr('aria-expanded', true)
-      $(`#submenu-${val}`).addClass("show")
-
-    })
-
-    $(`#${val}`).mouseleave(function () {
-      $(this).attr('aria-expanded', true)
-      $(`#submenu-${val}`).removeClass("show")
-    })
+  $("#close-sidebar").click(function(){
+      $('.navbar-toggler').toggleClass('collapsed')
+      $("#column_left").toggle()
   })
 
-  $('.multilevel').hover(function () {
-    console.log($(this))
-    let val = $($(this)).attr('id')
-    console.log(val, 'val')
-
-    $(`#${val}`).mouseenter(function () {
-      $(this).attr('aria-expanded', true)
-      $(`#submenu-${val}`).addClass("show")
-
-    })
-
-    $(`#${val}`).mouseleave(function () {
-      $(this).attr('aria-expanded', true)
-      $(`#submenu-${val}`).removeClass("show")
-    })
+  $(".navbar-toggler").click(function(e){
+      e.stopPropagation();
+      e.preventDefault();
+      $(this).toggleClass('collapsed')
+      $("#column_left").toggle()
   })
 
-  $(".multi-column-img-dropdown").hover(function () {
-    $(this).addClass('translateImage')
-  },
-    function () {
-      $(this).removeClass('translateImage')
-    }
+  $(".nav-link").click(function(){
+      $(this).toggleClass('addcolor')
+  })    
+  $(".nav-link").blur(function(){
+      $(this).removeClass('addcolor')
+  })
+
+  $('.multilevel').each(function() {
+      let val = $($(this)).attr('id')
+
+      $(`#${val}`).mouseenter(function(){
+          $(this).attr('aria-expanded',true)
+          $(`#submenu-${val}`).addClass("show")
+          
+      })
+  
+      $(`#${val}`).mouseleave(function(){
+          $(this).attr('aria-expanded',true)
+          $(`#submenu-${val}`).removeClass("show")
+      })    
+  })
+
+  $('.multilevel').hover(function() {
+      console.log($(this))
+      let val = $($(this)).attr('id')
+      console.log(val,'val')
+
+      $(`#${val}`).mouseenter(function(){
+          $(this).attr('aria-expanded',true)
+          $(`#submenu-${val}`).addClass("show")
+          
+      })
+  
+      $(`#${val}`).mouseleave(function(){
+          $(this).attr('aria-expanded',true)
+          $(`#submenu-${val}`).removeClass("show")
+      })    
+  })
+
+  $(".multi-column-img-dropdown").hover(function(){
+      $(this).addClass('translateImage')
+      },
+      function()
+      {
+          $(this).removeClass('translateImage')
+      }
   )
-
-
 
 });
 
